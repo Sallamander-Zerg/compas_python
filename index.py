86b92f4c0c5ed469dc2fef272bb09d7b9bb32c00
SALL <62800760+Sallamander-Zerg@users.noreply.github.com>
2022-07-27 00:20:11 +0700

    Def commit

--- index.py
+++ index.py
@@ -3,17 +3,30 @@ import sqlite3
 con = sqlite3.connect('local.db')
 print("введите ваш token для доступа к Dadata")
 cur = con.cursor()
-cur.execute('''CREATE TABLE IF NOT EXISTS DadataUsers (programming_language,token)''')
-pl =  input()
-token = input()
-cur.execute("INSERT INTO DadataUsers (programming_language,token) VALUES (?,?)",(pl,token))
-con.commit()
-dadata = Dadata(token)
-print("введите нужное вам место положение")
-zapros = dadata.suggest(name="address", query=input())
-for i in range(len(zapros)):
-    print(i,zapros[i]['value'])
-print("уточните ваш адрес")
-result=dadata.clean(name="address", source=input())
-print(result)
+cur.execute('''CREATE TABLE IF NOT EXISTS DadataUsers (Dadata_URL_servis,programming_language,token)''')
+print("выберете язык введите влучаи неверного выбора програма закроеться")
+print("ru для исползования Русского")
+print("en to use Еnglish")
+def compas(dadata):    
+    print("введите нужное вам место положение")
+    zapros = dadata.suggest(name="address", query=input(),language=lang)
+    for i in range(len(zapros)):
+        print(i,zapros[i]['value'])
+    print("уточните ваш адрес")
+    result=dadata.clean(name="address", source=input())
+    print(result[0]["geo_lat"]["geo_lon"])
+
+lang =  input()
+token = input() 
+def database(lang,token):
+    if lang=="en" or lang=="ru":
+        cur.execute("INSERT INTO DadataUsers (Dadata_URL_servis,programming_language,token) VALUES ('https://dadata.ru',?,?)",(lang,token))
+        con.commit()
+        while True: 
+            dadata = Dadata(token) 
+            compas(dadata)     
+            continue
+    else:
+        print("ошибка языка")    
+database(lang,token) 
     

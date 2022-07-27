3a60c80b8d3064fbedca0bf41ed2e74becb1a742
SALL <62800760+Sallamander-Zerg@users.noreply.github.com>
2022-07-27 15:57:30 +0700

    final commit

--- index.py
+++ index.py
@@ -1,32 +1,37 @@
+from itertools import count
 from dadata import Dadata
 import sqlite3
 con = sqlite3.connect('local.db')
-print("введите ваш token для доступа к Dadata")
+
 cur = con.cursor()
-cur.execute('''CREATE TABLE IF NOT EXISTS DadataUsers (Dadata_URL_servis,programming_language,token)''')
-print("выберете язык введите влучаи неверного выбора програма закроеться")
-print("ru для исползования Русского")
-print("en to use Еnglish")
+cur.execute('''CREATE TABLE IF NOT EXISTS DadataUsers (Dadata_URL_servis,programming_language,token,key)''')
+def database(lang,token,key):
+    if lang=="en" or lang=="ru":
+        cur.execute("INSERT INTO DadataUsers (Dadata_URL_servis,programming_language,token,key) VALUES ('https://dadata.ru',?,?,?)",(lang,token,key))
+        con.commit()
+        while True: 
+            dadata = Dadata(token,key) 
+            compas(dadata)     
+            continue
+    else:
+        print("ошибка языка")  
+          
 def compas(dadata):    
     print("введите нужное вам место положение")
     zapros = dadata.suggest(name="address", query=input(),language=lang)
     for i in range(len(zapros)):
-        print(i,zapros[i]['value'])
-    print("уточните ваш адрес")
-    result=dadata.clean(name="address", source=input())
-    print(result[0]["geo_lat"]["geo_lon"])
-
+        print(zapros[i]['value'])
+    print("уточните ваш адрес введя нужный вам адрес")
+    adress = input()
+    result=dadata.clean(name="address", source=adress)
+    print(result['geo_lat'])
+    print(result['geo_lon'])
+    
+print("введите ваш token для доступа к Dadata и выберете язык введите влучаи неверного выбора програма закроеться")
+print("ru для исползования Русского")
+print("en to use Еnglish")
 lang =  input()
 token = input() 
-def database(lang,token):
-    if lang=="en" or lang=="ru":
-        cur.execute("INSERT INTO DadataUsers (Dadata_URL_servis,programming_language,token) VALUES ('https://dadata.ru',?,?)",(lang,token))
-        con.commit()
-        while True: 
-            dadata = Dadata(token) 
-            compas(dadata)     
-            continue
-    else:
-        print("ошибка языка")    
-database(lang,token) 
+key = input()
+database(lang,token,key) 
     
